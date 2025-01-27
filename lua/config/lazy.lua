b2041e6ff2c07cf5dae499a8ab9e79b68c5374b7
Sahilb315 <bansalsahil315@gmail.com>
2025-01-27 18:33:55 +0530

    nvim init & neotree

---
+++ lua/config/lazy.lua
@@ -0,0 +1,44 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "--single-branch",
+    lazyrepo,
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  install = { 
+    colorscheme = { "tokyonight" },
+    missing = true,
+  },
+  checker = { 
+    enabled = true,
+    frequency = 86400,
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
